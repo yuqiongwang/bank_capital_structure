99d3fb8aa8eeed4a4e9d87a54b9b078a83a547d6
Yuqiong Wang <114857134+yuqiongwang@users.noreply.github.com>
2026-08-17 15:00:37 +0200

    Delete check_f_crossing.m

--- check_f_crossing.m
+++
@@ -1,9 +0,0 @@
-function [has_cross, fmin, fmax] = check_f_crossing(y, v, pi_max, params)
-    rho_L = params.rho - params.mu_L;
-    mu_star = params.r + pi_max .* (params.mu - params.r);
-    f = rho_L.* v - params.gamma + (params.mu_L - mu_star).* y;
-    fmin = min(f);
-    fmax = max(f);
-    has_cross = (fmin <= 0) && (fmax >= 0);
-end
-
